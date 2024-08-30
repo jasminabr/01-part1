28dc6ac064f12c599fd3360ef24ce549580fc345
first_name last_name <email_address>
2024-08-30 10:14:18 +0300

    Node-pakettigh-pages asennettu

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,50 @@
-/*import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const Hello = (props) => {
+  console.log(props);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+        Hello {props.name},from {props.city} you are {props.age} years old
       </p>
-    </>
-  );
-}
-
-
-kjo ishte nga guida e internetit 
-
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
-export default App;
-*/
-
-const App = () => {
-  console.log("Hello from komponentti App");
-  return (
-    <>
-      <sction>
-        <p>App-komponentissa on sisaltö : Hello world!</p>
-      </sction>
-    </>
+    </div>
   );
 };
+
+const footer = () => {
+  return (
+    <div>
+      greeting app created by
+      <a href="https://github.com/jasminabr/">Jasminabr</a>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
+
+  const nimi = "Pekka";
+  const ika = 10;
+  const kaupunki = "Helsinki";
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <hr></hr>
+
+      <section>
+        <h1>Greetings</h1>
+        <Hello name="Jasmina" age={25 + 3} />
+        <footer />
+        <Hello name={nimi} city={kaupunki} age={ika} />
+      </section>
+      <hr></hr>
+    </div>
+  );
+};
+
 export default App;
